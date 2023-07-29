c5111fc07213356c78688c0c23ac82b711c1636e
Dmitry Ababkov <ababkov@yandex.ru>
2023-07-30 02:24:17 +0500

    Task 3 ready

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,26 @@
+const holes = document.querySelectorAll(".hole"),
+  dead = document.querySelector("#dead"),
+  lost = document.querySelector("#lost");
+
+function checkWinner() {
+  holes.forEach((item) => {
+    item.addEventListener("click", function () {
+      if (item.classList.contains("hole_has-mole")) {
+        if (dead.textContent == 10) {
+          alert("Победа");
+          location.reload();
+        } else {
+          dead.textContent = +dead.textContent + 1;
+        }
+      } else {
+        if (lost.textContent == 5) {
+          alert("Поражение!");
+          location.reload();
+        } else {
+          lost.textContent = +lost.textContent + 1;
+        }
+      }
+    });
+  });
+}
+checkWinner();
