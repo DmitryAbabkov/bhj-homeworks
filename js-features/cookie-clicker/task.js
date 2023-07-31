35c961a8f75c1742ea6d75f6557081a3c9f74d27
Dmitry Ababkov <ababkov88@yandex.ru>
2023-07-31 23:26:40 +0500

    All task ready

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,17 +1,14 @@
 let cookie = document.querySelector(".clicker__cookie"),
-  counter = document.querySelector("#clicker__counter");
-//   currentDate = new Date(),
-//   currentMinutes = currentDate.getHours(),
-//   currentSeconds = currentDate.getSeconds(),
-//   result = `${currentMinutes}:${currentSeconds}`;
+	cookieSpeed = document.querySelector('#clicker__speed'),
+	counter = document.querySelector("#clicker__counter");
 
-// let arr = [];
 
-cookie.addEventListener("click", function () {
-  counter.textContent = +counter.textContent + 1;
-  if (counter.textContent % 2 == 0) {
-    cookie.width = 200;
-  } else {
-    cookie.width = 300;
-  }
+cookie.addEventListener("click", function() {
+	counter.textContent = +counter.textContent + 1;
+	if (counter.textContent % 2 == 0) {
+		cookie.width = 200;
+	} else {
+		cookie.width = 300;
+	}
+	return;
 });
--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,12 +1,34 @@
-let timer = document.querySelector("#timer");
+let timer = document.querySelector("#timer"),
+	getHourse = +timer.textContent.slice(0, 2),
+	getMinutes = +timer.textContent.slice(3, 5),
+	getSeconds = +timer.textContent.slice(6, 8);
+
+// Base Task
+// let changeTimer = setInterval(() => {
+//   if (timer.textContent > 0) {
+//     timer.textContent -= 1;
+//   } else {
+//     clearInterval(changeTimer);
+//     alert("Вы победили в конкурсе!");
+//   }
+// }, 1000);
+
 
 let changeTimer = setInterval(() => {
-  if (timer.textContent > 0) {
-    timer.textContent -= 1;
-  } else {
-    clearInterval(changeTimer);
-    alert("Вы победили в конкурсе!");
-  }
-}, 1000);
+	if (getSeconds > 0) {
+		getSeconds -= 1;
 
-console.log(new Date());
+	} else if (getMinutes > 0 && getSeconds == 0) {
+		getMinutes -= 1;
+		getSeconds = 59;
+	} else if (getHourse > 0 && getMinutes == 0) {
+		getHourse -= 1;
+		getMinutes = 59;
+		getSeconds = 59;
+	}
+	let formatHourse = getHourse < 10 ? `0${getHourse}` : getHourse,
+		formatMinutes = getMinutes < 10 ? `0${getMinutes}` : getMinutes,
+		formatSeconds = getSeconds < 10 ? `0${getSeconds}` : getSeconds;
+
+	timer.textContent = `${formatHourse}:${formatMinutes}:${formatSeconds}`;
+}, 1000);
