ec2505c4b6d0860e90f7bc36181599dfe6139311
Dmitry Ababkov <ababkov88@yandex.ru>
2023-08-06 23:50:05 +0500

    Task 3 ready

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,73 +1,54 @@
-// const tabs = document.querySelectorAll(".tab"),
-//   tabContent = document.querySelectorAll(".tab__content");
-
-// function hideTab() {
-//   tabs.forEach((tab) => {
-//     tab.classList.remove("tab_active");
-//   });
-// }
-
-// function showContent(i) {
-//   let arr = Array.from(tabContent);
-//   arr.forEach((item) => {
-//     item.classList.remove("tab__content_active");
-//   });
-//   arr[i].classList.toggle("tab__content_active");
-// }
-
-// tabs.forEach((item, i) => {
-//   item.addEventListener("click", () => {
-//     hideTab();
-//     item.classList.add("tab_active");
-//     showContent(i);
-//   });
-// });
-
-// document.addEventListener("keydown", (e) => {
-//   if (e.code === "KeyW") {
-//     for (let i = 0; i < 6; i++) {
-//       showContent(i);
-//     }
-//   }
-// });
-
 const tabs = Array.from(document.querySelectorAll(".tab")),
-  tabContent = Array.from(document.querySelectorAll(".tab__content"));
+	tabContent = Array.from(document.querySelectorAll(".tab__content"));
 
 function hideTab() {
-  tabs.forEach((tab) => {
-    tab.classList.remove("tab_active");
-  });
+	tabs.forEach((tab) => {
+		tab.classList.remove("tab_active");
+	});
 }
 
 function showContent(i) {
-  tabContent.forEach((item) => {
-    item.classList.remove("tab__content_active");
-  });
-  tabContent[i].classList.add("tab__content_active");
+	tabContent.forEach((item) => {
+		item.classList.remove("tab__content_active");
+	});
+	tabContent[i].classList.add("tab__content_active");
 }
 
 tabs.forEach((item, i) => {
-  item.addEventListener("click", () => {
-    hideTab();
-    item.classList.add("tab_active");
-    showContent(i);
-  });
+	item.addEventListener("click", () => {
+		hideTab();
+		item.classList.add("tab_active");
+		showContent(i);
+	});
 });
 
 let counter = 0;
 
+function keyRemoveActive() {
+	tabs[counter].classList.remove("tab_active");
+	tabContent[counter].classList.remove("tab__content_active");
+}
+
+function keyAddActive() {
+	tabs[counter].classList.add("tab_active");
+	tabContent[counter].classList.add("tab__content_active");
+}
+
 document.addEventListener("keydown", (e) => {
-  if (e.code === "KeyW") {
-    if (counter <= tabs.length) {
-      tabs[counter].classList.remove("tab_active");
-      tabContent[counter].classList.remove("tab__content_active");
-      ++counter;
-      tabs[counter].classList.add("tab_active");
-      tabContent[counter].classList.add("tab__content_active");
-      console.log(counter);
-    } else {
-      return (counter = 0);
-    }
-  }
+	if (e.code === "KeyW") {
+		if (counter < tabs.length - 1) {
+			keyRemoveActive();
+			++counter;
+			keyAddActive();
+		}
+		return counter;
+	}
+
+	if (e.code === "KeyS") {
+		if (counter != 0) {
+			keyRemoveActive();
+			--counter;
+			keyAddActive();
+		}
+	}
 });
