2ab8b51b5adcf19cd36076fc668448d2f4ca4003
Дмитрий Абабков <dmitryababkov@MacBook-Air-Dmitrij.local>
2023-08-18 01:54:01 +0500

    All task reaady

---
+++ document-structure/cart/task.js
@@ -0,0 +1,74 @@
+const counterRemove = document.querySelectorAll('.product__quantity-control_dec'),
+	counterAdd = document.querySelectorAll('.product__quantity-control_inc'),
+	valueCounter = document.querySelectorAll('.product__quantity-value'),
+	cart = document.querySelector('.cart__products'),
+	cartAdd = document.querySelectorAll('.product__add'),
+	image = document.querySelectorAll('.product__image'),
+	product = document.querySelectorAll('.product'),
+	divCart = document.querySelector('.cart'),
+	cartProducts = Array.from(document.querySelector('.cart__products'));
+let arrVendorCode = [];
+
+divCart.style.display = 'none';
+
+counterAdd.forEach((item, index) => {
+	item.addEventListener('click', () => {
+		const currentValue = +valueCounter[index].textContent;
+		valueCounter[index].textContent = currentValue + 1;
+	});
+});
+
+counterRemove.forEach((item, index) => {
+	item.addEventListener('click', () => {
+		const currentValue = +valueCounter[index].textContent;
+		if (currentValue > 1) {
+			valueCounter[index].textContent = currentValue - 1;
+		}
+	});
+});
+
+function generationCart(srcImage, count, data) {
+	divCart.style.display = 'block';
+	const newProduct = document.createElement('div');
+	newProduct.classList.add('cart__product');
+	newProduct.setAttribute('data-id', data);
+	cart.appendChild(newProduct);
+	const productInfo = `
+    <div class="cart__product-remove">❌</div>
+    <img class="cart__product-image" src="${srcImage}">
+    <div class="cart__product-count">${count}</div>`;
+	newProduct.insertAdjacentHTML('beforeend', productInfo);
+	arrVendorCode.push(data);
+}
+
+cartAdd.forEach((item, i) => {
+	item.addEventListener('click', () => {
+		if (arrVendorCode.includes(product[i].dataset.id)) {
+			const currentValue = +valueCounter[i].textContent;
+			const existingValue = +document.querySelector(`[data-id="${product[i].dataset.id}"] .cart__product-count`).textContent;
+			document.querySelector(`[data-id="${product[i].dataset.id}"] .cart__product-count`).textContent = existingValue + currentValue;
+		} else {
+			generationCart(image[i].src, valueCounter[i].textContent, product[i].dataset.id);
+			setupRemoveListeners();
+		}
+	});
+});
+
+function setupRemoveListeners() {
+	const removeButtons = document.querySelectorAll('.cart__product-remove');
+	removeButtons.forEach((button) => {
+		button.addEventListener('click', removeCartItem);
+	});
+}
+
+function removeCartItem() {
+	const cartProduct = this.closest('.cart__product');
+	const dataId = cartProduct.getAttribute('data-id');
+	const index = arrVendorCode.indexOf(dataId);
+	if (index !== -1) {
+		arrVendorCode.splice(index, 1);
+	}
+	cartProduct.remove();
+}
+
+setupRemoveListeners();
--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,12 +1,12 @@
 const form = document.querySelector('form'),
-    input = document.querySelector('.tasks__input'),
-    list = document.querySelector('.tasks__list');
+	input = document.querySelector('.tasks__input'),
+	list = document.querySelector('.tasks__list');
+let counterArr = localStorage.length;
+let currentId = 0;
 
-    let counter = 0;
-
-function generationContent(value) {
-    list.insertAdjacentHTML('beforeend', `
-    <div class="task">
+function generationContent(value, currentId) {
+	list.insertAdjacentHTML('beforeend', `
+    <div class="task" data-id="${currentId}">
     <div class="task__title">
         ${value}
     </div>
@@ -14,44 +14,38 @@ function generationContent(value) {
 </div>
 `);
 }
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    if(input.value != '') {
-        generationContent(input.value);
-        ++counter;   
-        localStorage.setItem(`${arr.length+1}`, `${input.value}`); 
-           
-            // localStorage.setItem(`${++counter}`, `${input.value}`);
-    }
 
-console.log(counter);
-    form.reset();
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	if (input.value != '') {
+		generationContent(input.value, currentId);
+		localStorage.setItem(`${currentId}`, `${input.value}`);
+		++currentId;
+	}
+	form.reset();
 });
 
+let arr = [];
 
+function generationTasks() {
+	for (let i = 0; i < localStorage.length; i++) {
+		let key = localStorage.key(i);
+		arr.push(localStorage.getItem(key));
+	}
+	arr.reverse();
+	arr.forEach(item => {
+		generationContent(item, currentId);
+		++currentId;
+	});
+}
 
 list.addEventListener('click', (e) => {
-    if (e.target.classList.contains('task__remove')) {
-        e.target.closest('.task').remove();
-    }
+	if (e.target.classList.contains('task__remove')) {
+		const taskElement = e.target.closest('.task');
+		const taskId = taskElement.getAttribute('data-id');
+		taskElement.remove();
+		localStorage.removeItem(taskId);
+	}
 });
-
-let arr =[];
-function generationTasks() {
-for(let i=0; i<localStorage.length; i++) {
-  let key = localStorage.key(i);
-  arr.push(localStorage.getItem(key));
-}
-arr.reverse();
-arr.forEach(item => {
-    generationContent(item);
-});
-
-}
 
 document.addEventListener('DOMContentLoaded', generationTasks());
-
-
-// Осталось победить: удаление и перезапись в localStorage
-
-// localStorage.clear();
